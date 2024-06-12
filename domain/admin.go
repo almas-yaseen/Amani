c9c8eee23d212b1eaa1785146896ea2539e04e69
almas <almasyaseen18@gmail.com>
2024-06-12 21:32:23 +0530

    new changes

--- domain/admin.go
+++ domain/admin.go
@@ -1,24 +1,23 @@
 package domain
 
 type Car struct {
-	ID           uint `gorm:"primaryKey;autoIncrement"`
-	Brand        string
-	Model        string
-	Year         string
-	Color        string
-	Variant      string
-	Kms          int
-	Ownership    int
-	Bannerimage  string
-	Transmission string
-	Images       []Image `gorm:"foreignKey:CarID"`
-	RegNo        string
-	Status       string
-	Price        int
+	ID           uint    `gorm:"primaryKey;autoIncrement" json:"id"`
+	Brand        string  `json:"brand"`
+	Model        string  `json:"model"`
+	Year         string  `json:"year"`
+	Color        string  `json:"color"`
+	Variant      string  `json:"variant"`
+	Kms          int     `json:"kms"`
+	Ownership    int     `json:"ownership"`
+	Bannerimage  string  `json:"bannerimage"`
+	Transmission string  `json:"transmission"`
+	Images       []Image `gorm:"foreignKey:CarID" json:"images"`
+	RegNo        string  `json:"regno"`
+	Status       string  `json:"status"`
+	Price        int     `json:"price"`
 }
-
 type Image struct {
-	ID    uint `gorm:"primaryKey:autoIncrement"`
-	CarID uint `gorm:"index"`
-	Path  string
+	ID    uint   `gorm:"primaryKey;autoIncrement" json:"id"`
+	CarID uint   `json:"car_id"`
+	Path  string `json:"path"`
 }
