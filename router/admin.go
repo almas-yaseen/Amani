a7cf793c1f518d5c4dc3959667ba390956d933e2
almas <almasyaseen18@gmail.com>
2024-06-12 16:31:15 +0530

    get all

--- router/admin.go
+++ router/admin.go
@@ -10,8 +10,9 @@ import (
 func AdminRoutes(r *gin.RouterGroup, db *gorm.DB) *gin.RouterGroup {
 
 	r.GET("/admin", handlers.Dashboard(db))
-	r.GET("/myapp/get_banner_images", handlers.Get_Banner_Vehicles(db))
+	r.GET("/myapp/Get_Banner_vehicles", handlers.Get_Banner_Vehicles(db))
 	r.GET("/myapp/get_all_vehicles", handlers.GetAllVehicles(db))
+	r.GET("/myapp/get_specific_vehicle/:id")
 
 	admin := r.Group("/admin")
 	{
