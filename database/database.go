9eb597d1d497bd111de5b80547aae3fe87f655f7
fayasPA <fayasmuthalib@gmail.com>
2024-06-13 17:10:01 +0530

    first

--- database/database.go
+++ database/database.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 
 func ConnectDatabase(cfg config.Config) (*gorm.DB, error) {
 
-	psqlInfo := fmt.Sprintf("host=localhost user=%s dbname=%s  port=%s password=%s ", cfg.DBUser, cfg.DBName, cfg.DBPort, cfg.DBPassword)
+	psqlInfo := fmt.Sprintf("host=localhost user=%s dbname=%s  port=%s password=%s sslmode=disable TimeZone=Asia/Shanghai", cfg.DBUser, cfg.DBName, cfg.DBPort, cfg.DBPassword)
 
 	db, dberr := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
 
