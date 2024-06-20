f205452423fe06554b2e5c9118ec2f9dc1f15b27
fayasPA <fayasmuthalib@gmail.com>
2024-06-21 00:35:02 +0530

    check

--- main.go
+++ main.go
@@ -11,9 +11,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// func CORSMiddleware() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://www.amanimotors.in")
+// 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization")
+
+// 		if c.Request.Method == "OPTIONS" {
+// 			c.AbortWithStatus(http.StatusOK)
+// 			return
+// 		}
+
+// 		c.Next()
+// 	}
+// }
+
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://www.amanimotors.in")
+		origin := c.Request.Header.Get("Origin")
+		if origin == "https://www.amanimotors.in" || origin == "http://localhost:5173" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization")
